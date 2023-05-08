532d1e51d8070ec504b4634ac9a18a5c6fe2917b
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2023-05-08 09:13:23 -0300

    Initial commit - Creating text component

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import Text from "@/components/atom/Text";
+
+import { TextColors } from "@/components/atom/Text/types";
+
+export default function Home() {
+  const colorValueArray = Object.keys(TextColors);
+
+  return (
+    <main>
+      <Text size="2xl" element="h1" weight="bold">
+        Hello World
+      </Text>
+      <Text
+        size="2xl"
+        element="h1"
+        weight="bold"
+        align="end"
+        decoration="underline"
+        transform="uppercase"
+      >
+        Hello World
+      </Text>
+      <Text>Colors</Text>
+
+      {colorValueArray.map((color) => (
+        <Text color={color as keyof typeof TextColors} size="2xl" weight="bold">
+          aA
+        </Text>
+      ))}
+    </main>
+  );
+}
---
+++ src/components/atom/Text/index.tsx
@@ -0,0 +1,54 @@
+import {
+  TextProps,
+  TextWeights,
+  TextSizes,
+  TextAligns,
+  TextDecorations,
+  TextTansforms,
+  TextColors,
+} from "./types";
+
+function TextElement(props: TextProps) {
+  const { element } = props;
+
+  if (element === "h1") return <h1 {...props}></h1>;
+  if (element === "h2") return <h2 {...props}></h2>;
+  if (element === "h3") return <h3 {...props}></h3>;
+  if (element === "h4") return <h4 {...props}></h4>;
+  if (element === "h5") return <h5 {...props}></h5>;
+  if (element === "h6") return <h6 {...props}></h6>;
+
+  if (element === "p") return <p {...props}></p>;
+  if (element === "pre") return <pre {...props}></pre>;
+  if (element === "small") return <small {...props}></small>;
+  if (element === "span") return <span {...props}></span>;
+  if (element === "caption") return <caption {...props}></caption>;
+
+  return <p {...props}></p>;
+}
+
+export default function Text({
+  children,
+  element,
+  className = "",
+  weight = "normal",
+  color = "darkColor",
+  align = "start",
+  decoration = "no-underline",
+  italic = false,
+  transform = "normal-case",
+  size = "base",
+}: TextProps) {
+  return (
+    <TextElement
+      className={`text ${TextSizes[size]} ${TextWeights[weight]} ${
+        TextAligns[align]
+      } ${TextColors[color]} ${TextDecorations[decoration]} ${TextTansforms[transform]} ${
+        italic ? "italic" : "not-italic"
+      } ${className || ""}`.trim()}
+      element={element}
+    >
+      {children}
+    </TextElement>
+  );
+}
