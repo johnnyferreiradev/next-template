f80d5ebf09bc5320e4ca7602e362760fb2bddd10
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2023-05-28 14:56:49 -0300

    Adding collapse in presentation page

--- src/app/documentation/page.tsx
+++ src/app/documentation/page.tsx
@@ -2,20 +2,29 @@
 
 import { useState } from 'react';
 
-import Checkbox from '@/components/atom/Checkbox';
+import Collapse from '@/components/atom/Collapse';
+import Text from '@/components/atom/Text';
 
 export default function Documentation() {
-  const [checked, setChecked] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <main className="p-10 h-screen flex justify-center items-center">
-      <Checkbox
-        checked={checked}
-        onChange={() => setChecked(!checked)}
-        className="mb-8"
+    <main className="p-10 h-screen flex justify-center pt-56">
+      <Collapse
+        collapseHeader="Clique aqui!"
+        isOpen={isOpen}
+        setIsOpen={() => setIsOpen(!isOpen)}
+        className="max-w-xs"
       >
-        Meu checkbox
-      </Checkbox>
+        <div className="flex">
+          <Text size="xl" className="mr-1">
+            Siga:
+          </Text>
+          <Text weight="bold" size="xl" color="secondary200">
+            @johnnyferreira09
+          </Text>
+        </div>
+      </Collapse>
     </main>
   );
 }
